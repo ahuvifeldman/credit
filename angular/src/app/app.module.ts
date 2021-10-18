e9b21573ea8618f6a378f5f07bf97fb11addda0c
ahuvi <92719342+ahuvifeldman@users.noreply.github.com>
2021-10-18 19:17:37 +0300

    Add files via upload

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule} from "@angular/forms"; 
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Route } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
+import { CreditComponent } from './credit/credit.component';
+import { CreditService } from './service/service-credit.service';
+
+
+/* const APP_ROUTES: Route[] = [
+  { path: "", pathMatch: "full", redirectTo: "home" },
+  { path: "home", component: HomeComponent }
+] */
+
+@NgModule({
+   //components
+	//direcive
+	//pipe
+	declarations: [
+      AppComponent,
+      CreditComponent
+   ],
+   //modules
+	imports: [
+      BrowserModule,
+      FormsModule,
+      ReactiveFormsModule,
+      HttpClientModule
+
+   ],
+   providers: [CreditService],
+   bootstrap: [AppComponent],
+})
+export class AppModule { }
